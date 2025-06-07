2c3ae653822c812d20bebfad31e08e17da26da52
Ryan Legler <ryan.legler@gmail.com>
2025-06-07 11:31:16 -0700

    Disables explicit any eslint rule
    Turns off the `@typescript-eslint/no-explicit-any` ESLint rule.
    This allows the usage of `any` type without causing errors,
    providing more flexibility during development.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,13 @@ const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')];
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off' // or use 'warn' if you want warnings instead of errors
+    }
+  }
+];
 
 export default eslintConfig;
